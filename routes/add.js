9b5c24d24a2068f3b16b01dc8cc5f9b2af85f86a
ruchikas <ruchikas300@gmail.com>
2016-02-04 11:40:51 -0800

    updated js

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
+var newFriend;
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) {
+	res.render('add');
+
+	var name = req.query.name;
+	var description = req.query.description;
+	 newFriend = {
+		"name": name,
+		"description":description,
+		"imageURL": 'http://lorempixel.com/400/400/people'
+	};
+	data["friends"].push(newFriend);
+	
+}
